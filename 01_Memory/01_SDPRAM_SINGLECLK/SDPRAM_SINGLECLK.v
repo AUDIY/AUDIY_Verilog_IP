6ae9e4562ea1fbe9000479d4ac95e162462a217f
AUDIY <96096729+AUDIY@users.noreply.github.com>
2024-02-28 21:13:43 +0900

    Update SDPRAM_SINGLECLK.v
    Change Copyright year.

--- 01_Memory/01_SDPRAM_SINGLECLK/SDPRAM_SINGLECLK.v
+++ 01_Memory/01_SDPRAM_SINGLECLK/SDPRAM_SINGLECLK.v
@@ -27,7 +27,7 @@
 *
 * License under CERN-OHL-P v2
 --------------------------------------------------------------------------------
-| Copyright AUDIY 2023.                                                        |
+| Copyright AUDIY 2023 - 2024.                                                 |
 |                                                                              |
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
