e3ee7eb0dcb0550dcaf1ec571a8d3c181f3a95b8
AUDIY <96096729+AUDIY@users.noreply.github.com>
2026-04-26 21:00:29 +0900

    Add files via upload

--- DFF_SYNC/DFF_SYNC.v
+++ DFF_SYNC/DFF_SYNC.v
@@ -1,11 +1,11 @@
 /*-----------------------------------------------------------------------------
 * DFF_SYNC.v
 *
-* DFF Synchronizer for asynchronous 1bit signal
+* D-FF Synchronizer for asynchronous signal
 *
-* Version: 0.10
+* Version: 0.20
 * Author : AUDIY
-* Date   : 2026/04/19
+* Date   : 2026/04/26
 *
 * Port
 *   Input
@@ -17,6 +17,7 @@
 *       DATA_O   : Synchronized Data
 *
 * Parameter
+*   BIT_WIDTH : Input/Output Data Bit Width.
 *   OUT_STAGE : Output Synchronizer Stage
 *
 * License
@@ -37,25 +38,41 @@
 `default_nettype none
 
 module DFF_SYNC #(
+    parameter BIT_WIDTH = 4,
     parameter OUT_STAGE = 2
 ) (
-    input  wire INCLK_I,
-    input  wire DATA_I,
-    input  wire OUTCLK_I,
-    output wire DATA_O
+    input  wire                     INCLK_I,
+    input  wire [(BIT_WIDTH - 1):0] DATA_I,
+    input  wire                     OUTCLK_I,
+    output wire [(BIT_WIDTH - 1):0] DATA_O
 );
     
     localparam RIGHT_OUT_STAGE = (OUT_STAGE < 2) ? 2 : OUT_STAGE;
 
-    reg                           INDATA  = 1'b0;
-    reg [(RIGHT_OUT_STAGE - 1):0] OUTDATA = {(RIGHT_OUT_STAGE){1'b0}};
+    reg [(BIT_WIDTH - 1):0] INDATA = {(BIT_WIDTH){1'b0}};
+    reg [(BIT_WIDTH - 1):0] OUTDATA [(RIGHT_OUT_STAGE - 1):0];
 
+    integer i;
+    integer j;
+
+    initial begin
+        for (i = 0; i < RIGHT_OUT_STAGE; i = i + 1) begin
+            OUTDATA[i] = {(BIT_WIDTH){1'b0}};
+        end
+    end
+
+    /* Input Clock Domain */
     always @(posedge INCLK_I) begin
         INDATA <= DATA_I;
     end
 
+    /* Output Clock Domain (2 or more D-FF synchronize) */
     always @(posedge OUTCLK_I) begin
-        OUTDATA <= {OUTDATA[(RIGHT_OUT_STAGE - 2):0], INDATA};
+        OUTDATA[0] <= INDATA;
+
+        for (j = 1; j < RIGHT_OUT_STAGE; j = j + 1) begin
+            OUTDATA[j] <= OUTDATA[j - 1];
+        end
     end
 
     assign DATA_O = OUTDATA[RIGHT_OUT_STAGE - 1];
